310e954ef56a8f10970d340e1e57ed2f62dbc058
kwy <kwy1762@naver.com>
2025-05-01 10:51:30 +0900

    description

---
+++ app/routes/pages/home/components/description.tsx
@@ -0,0 +1,15 @@
+export default function Description() {
+  return (
+    <section>
+      <div className="flex h-[600px] items-center justify-center bg-[#F9FAFB]">
+        <p className="py-[200px] text-center text-[32px] leading-[1.6] font-[700] text-[#191F28]">
+          내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
+          <br />
+          이제껏 경험 못 했던 쉽고 편리한 금융 서비스,
+          <br />
+          토스와 함께라면 당신의 일상이 새로워질 거예요.
+        </p>
+      </div>
+    </section>
+  );
+}
