cd06b49a2099da229ccc73aadd52554ca28ee1f2
kwy <kwy1762@naver.com>
2025-05-07 10:14:37 +0900

    hero animation

--- app/routes/pages/home/components/hero.tsx
+++ app/routes/pages/home/components/hero.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'motion/react';
+
 import HeroDown from '~/components/svg/hero-down.svg?react';
 
 import AppButton from './app-button';
@@ -6,7 +8,14 @@ export default function Hero() {
   return (
     <section className="relative flex h-[calc(100vh-60px)] w-full flex-col items-center bg-[url('/images/new_main.png')] bg-cover bg-center pt-[120px]">
       <div className="hero-shadow absolute top-0 h-[500px] w-full" />
-      <div className="absolute z-10">
+      <motion.div
+        className="absolute z-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 1, ease: 'easeOut' }}
+        // duration: 애니메이션 재생 시간
+        // ease: 디폴트는 선형적, 지정하면 곡선형태로 애니메이션 (별 차이 안느껴짐)
+      >
         <h1 className="text-center text-[66px] leading-[1.4] font-[700]">
           금융의 모든 것<br />
           토스에서 쉽고 간편하게
@@ -21,10 +30,34 @@ export default function Hero() {
             Google Play
           </AppButton>
         </div>
-      </div>
-      <div className="absolute bottom-[30px] z-10">
+      </motion.div>
+      <motion.div
+        className="absolute bottom-[45px] z-10"
+        initial={{ opacity: 0, scale: 0.85 }}
+        animate={{ opacity: 1, y: [0, 15, 0], scale: 1 }}
+        transition={{
+          opacity: {
+            duration: 1,
+            repeat: Infinity,
+            repeatType: 'reverse',
+            ease: 'easeOut',
+          },
+          scale: {
+            duration: 1,
+            repeat: Infinity,
+            repeatType: 'reverse',
+            ease: 'easeInOut',
+          },
+          y: {
+            duration: 2,
+            repeat: Infinity,
+            repeatType: 'loop',
+            ease: 'easeInOut',
+          },
+        }}
+      >
         <HeroDown width={50} height={50} />
-      </div>
+      </motion.div>
     </section>
   );
 }
