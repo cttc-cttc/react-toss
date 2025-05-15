046a2db09a5490851620688455d27b78202b4d9b
kwy <kwy1762@naver.com>
2025-05-15 09:49:07 +0900

    description 섹션 반응형 적용

--- app/routes/pages/home/components/description.tsx
+++ app/routes/pages/home/components/description.tsx
@@ -1,14 +1,36 @@
+import useMediaQuery, { MediaQuery } from '~/hooks/use-media-query';
+
 export default function Description() {
+  const isMobile = useMediaQuery(MediaQuery.MOBILE);
+
   return (
     <section>
-      <div className="flex h-[600px] items-center justify-center bg-[#F9FAFB]">
-        <p className="py-[200px] text-center text-[32px] leading-[1.6] font-[700] text-[#191F28]">
-          내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
-          <br />
-          이제껏 경험 못 했던 쉽고 편리한 금융 서비스,
-          <br />
-          토스와 함께라면 당신의 일상이 새로워질 거예요.
-        </p>
+      <div className="flex h-[428px] items-center justify-center bg-[#F9FAFB] desktop:h-[600px]">
+        <div className="container">
+          {isMobile ? (
+            <p className="py-[100px] text-center text-[22px] leading-[1.6] font-[600] text-[#191F28]">
+              내 모든 금융 내역을 한눈에
+              <br />
+              조회하고 한 곳에서 관리하세요.
+              <br />
+              이제껏 경험 못 했던
+              <br />
+              쉽고 편리한 금융 서비스,
+              <br />
+              토스와 함께라면
+              <br />
+              당신의 일상이 새로워질 거예요.
+            </p>
+          ) : (
+            <p className="text-center leading-[1.6] font-[700] text-[#191F28] desktop:py-[200px] desktop:text-[32px]">
+              내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
+              <br />
+              이제껏 경험 못 했던 쉽고 편리한 금융 서비스,
+              <br />
+              토스와 함께라면 당신의 일상이 새로워질 거예요.
+            </p>
+          )}
+        </div>
       </div>
     </section>
   );
