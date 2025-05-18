56a40f6ee72188081e958d3256361be431b35a55
kwy <kwy1762@naver.com>
2025-05-18 17:10:47 +0900

    payment-service 반응형 수정

--- app/routes/pages/home/components/payment-service.tsx
+++ app/routes/pages/home/components/payment-service.tsx
@@ -12,7 +12,7 @@ export default function PaymentService() {
             <h1 className="mb-[10px] text-[18px] leading-[1.3] font-[700] text-[#3182f6] desktop:mb-[30px] desktop:text-[28px]">
               결제
             </h1>
-            <h2 className="mb-[60px] text-[28px] leading-[1.4] font-[700] text-[#35445a] desktop:mb-[80px] desktop:text-[50px]">
+            <h2 className="mb-[10px] text-[28px] leading-[1.4] font-[700] text-[#35445a] desktop:mb-[90px] desktop:text-[50px]">
               결제는 간편하게,
               <br />
               할인과 적립은 두둑히
@@ -28,9 +28,9 @@ export default function PaymentService() {
             )}
           </InDropUp>
           <InDropUp>
-            <div className="relative">
+            <div className="relative flex justify-center">
               <img
-                className="absolute top-[60px] left-[80px] z-10 w-[215px] rounded-[30px] desktop:top-[90px] desktop:left-[-2px] desktop:w-[300px]"
+                className="absolute top-[60px] left-auto z-10 w-[215px] rounded-[30px] desktop:top-[90px] desktop:left-[-2px] desktop:w-[300px]"
                 src="/images/payment-01.png"
                 alt="payment-01"
               />
@@ -43,12 +43,9 @@ export default function PaymentService() {
           </InDropUp>
           {isMobile && (
             <InDropUp>
-              <p className="text-[17px] leading-[1.5] font-[600] text-[#4e5968]">
-                온라인과 오프라인 모두
-                <br />
-                국내는 물론, 해외에서도
-                <br />
-                토스로 간편하게 결제해요.
+              <p className="mb-[32px] flex justify-center text-center text-[17px] leading-[1.5] font-[600] text-[#4e5968]">
+                온라인과 오프라인 모두 국내는 물론, <br />
+                해외에서도 토스로 간편하게 결제해요.
               </p>
             </InDropUp>
           )}
@@ -57,7 +54,7 @@ export default function PaymentService() {
           <InDropUp>
             <div className="relative flex justify-center">
               <img
-                className="absolute top-[60px] left-[150px] z-10 w-[215px] rounded-[30px] desktop:top-[90px] desktop:left-[162px] desktop:w-[300px]"
+                className="absolute top-[60px] left-auto z-10 w-[215px] rounded-[30px] desktop:top-[90px] desktop:left-[162px] desktop:w-[300px]"
                 src="/images/payment-02.png"
                 alt="payment-02"
               />
@@ -69,13 +66,21 @@ export default function PaymentService() {
             </div>
           </InDropUp>
           <InDropUp className="relative text-[17px] leading-[1.5] font-[600] text-[#4e5968] desktop:text-[23px]">
-            <p className="absolute top-[50px] left-[150px]">
-              결제할 땐, 혜택이 빠질 수 없죠.
-              <br />
-              쿠폰과 포인트 써서 할인 받고
-              <br />
-              토스프라임 적립까지 또 받아요.
-            </p>
+            {!isMobile ? (
+              <p className="absolute top-[50px] left-[150px]">
+                결제할 땐, 혜택이 빠질 수 없죠.
+                <br />
+                쿠폰과 포인트 써서 할인 받고
+                <br />
+                토스프라임 적립까지 또 받아요.
+              </p>
+            ) : (
+              <p className="mb-[32px] flex justify-center text-center">
+                결제할 땐, 혜택이 빠질 수 없죠. 쿠폰과 포인트
+                <br />
+                써서 할인 받고 토스프라임 적립까지 또 받아요.
+              </p>
+            )}
           </InDropUp>
         </div>
       </div>
