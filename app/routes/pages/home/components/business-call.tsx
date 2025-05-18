5286e5ebaf16abbc149229d2c89ed8af13ec1695
kwy <kwy1762@naver.com>
2025-05-18 17:29:30 +0900

    business-call 반응형 수정

--- app/routes/pages/home/components/business-call.tsx
+++ app/routes/pages/home/components/business-call.tsx
@@ -5,14 +5,14 @@ export default function BusinessCall() {
   const isMobile = useMediaQuery(MediaQuery.MOBILE);
 
   return (
-    <section className="h-[306px] bg-[#f5f6f8] py-[33px]">
+    <section className="h-[306px] bg-[#f5f6f8] py-[72px] desktop:py-[33px]">
       <div className="m-auto flex w-[800px] justify-center">
         <div className="flex w-[92%] items-center justify-between">
           <div>
-            <h1 className="mb-[16px] text-[36px] leading-[1.3] font-[700] text-[#333d4b]">
+            <h1 className="mb-[12px] text-[30px] leading-[1.3] font-[700] text-[#00132b94] desktop:mb-[16px] desktop:text-[36px] desktop:text-[#333d4b]">
               비즈니스 고객 제보
             </h1>
-            <p className="mb-[24px] text-[18px] leading-[1.5] font-[400] text-[#00132b94]">
+            <p className="mb-[24px] text-[17px] leading-[1.5] font-[400] text-[#00132b94] desktop:text-[18px]">
               토스팀과의 협업 중 불편을 느끼셨나요?
               <br />
               지금 바로 알려주세요.
