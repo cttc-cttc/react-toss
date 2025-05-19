8d679d201b7b282b66720cdaf3a25d584a172907
kwy <kwy1762@naver.com>
2025-05-19 13:04:57 +0900

    관리자 공지사항 상세 페이지 수정버튼 추가

--- app/routes/pages/admin-notice-details/route.tsx
+++ app/routes/pages/admin-notice-details/route.tsx
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 import { useNavigate } from 'react-router';
 
 import prisma from '~/.server/lib/prisma';
+import { BreadcrumbItem } from '~/components/ui/breadcrumb';
 import { Button } from '~/components/ui/button';
 import { Separator } from '~/components/ui/separator';
 
@@ -21,6 +22,10 @@ export const loader = async ({ params }) => {
   return {
     notice,
   };
+};
+
+export const handle = {
+  breadcrumb: () => <BreadcrumbItem>공지사항 관리 / 상세</BreadcrumbItem>,
 };
 
 export default function AdminNoticeDetails({ loaderData }: Route.ComponentProps) {
@@ -43,8 +48,11 @@ export default function AdminNoticeDetails({ loaderData }: Route.ComponentProps)
           </p>
         ))}
       </div>
-      <div className="mt-8">
-        <Button onClick={() => navigate(-1)}>목록 보기</Button>
+      <div className="mt-8 flex justify-between">
+        <Button onClick={() => navigate(-1)} variant="secondary">
+          목록 보기
+        </Button>
+        <Button>수정</Button>
       </div>
     </div>
   );
